386559160c16adc55185e5593d036c5bb18a4cac
Fillip Hannisdal <fillip@dragonbyte-tech.com>
2017-03-08 19:02:10 +0000

    Formula fixes

--- xf-cli.rb
+++ xf-cli.rb
@@ -1,5 +1,5 @@
-class Delta < Formula
-	desc "diff tool with browser-based GUI"
+class XfCli < Formula
+	desc "CLI tool for XenForo 2"
 	homepage "https://github.com/JakeBooher/xf-cli"
 	url "https://github.com/JakeBooher/xf-cli/archive/1.0.0.tar.gz"
 	sha256 "78f9d12cc53f27b0628dbd9d408087727336ece5a2bbab5905e05c88cd44dda7"
